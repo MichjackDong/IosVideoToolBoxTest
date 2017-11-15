840cf325f327756a004fac05b3c9142ffffe351e
谢贺东 <XieHeDong@XHDong.local>
2017-11-15 13:58:06 +0800

    Initial Commit

---
+++ IosVideoToolBoxTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IosVideoToolBoxTest
+//
+//  Created by 谢贺东 on 2017/11/15.
+//  Copyright © 2017年 XHDong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IosVideoToolBoxTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  IosVideoToolBoxTest
+//
+//  Created by 谢贺东 on 2017/11/15.
+//  Copyright © 2017年 XHDong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
